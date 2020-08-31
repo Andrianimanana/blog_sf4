8a198b1cc110b5d606a6154be3521d602a7b74e1
Armel <arbandry@gmail.com>
2020-08-31 22:48:06 +0300

    entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libele;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastupdate;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Article", mappedBy="categories")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibele(): ?string
+    {
+        return $this->libele;
+    }
+
+    public function setLibele(string $libele): self
+    {
+        $this->libele = $libele;
+
+        return $this;
+    }
+
+    public function getLastupdate(): ?\DateTimeInterface
+    {
+        return $this->lastupdate;
+    }
+
+    public function setLastupdate(\DateTimeInterface $lastupdate): self
+    {
+        $this->lastupdate = $lastupdate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            $article->removeCategory($this);
+        }
+
+        return $this;
+    }
+}
