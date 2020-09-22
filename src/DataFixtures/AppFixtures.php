127b0191d3f772ed33e586ddbae083e784f291c0
Armel <arbandry@gmail.com>
2020-09-22 16:23:25 +0300

    use faker for DoctrineFixturesBundle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,65 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\Category;
+use App\Entity\Article;
+use App\Entity\Comment;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker;
 
 class AppFixtures extends Fixture
 {
+   private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // On configure dans quelles langues nous voulons nos données
+        $faker = Faker\Factory::create('fr_FR');
+        $categories = [];
+        for($i=1;$i<5;$i++){
+        	# category 
+        	$category 	=  new Category();
+        	$category->setLibele("Categorie ".$i);
+        	$category->setLastupdate($faker->dateTime);
+        	array_push($categories, $category);
+        	# user
+        	$roles = ["ROLE_USER", "ROLE_ADMIN"];
+        	$user 	  = new User();
+        	$password = $this->encoder->encodePassword($user, 'elle');
+        	$role = array_rand($roles);
+        	$user->addRoles($roles[$role]);
+        	$user->setEmail($faker->email);
+        	$user->setNameuser($faker->userName); 
+        	$user->setPassword($password);
+        	$manager->persist($user);       	
+        	$manager->persist($category);  
+        } 
 
-        $manager->flush();
+        for($i=1;$i<50;$i++){
+        	$article 	= new Article();
+        	$images 	= ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"]; 
+        	$image 		= array_rand($images);
+    		$article->setPicture($images[$image]);
+        	for($cat=1;$cat<rand(1,count($categories)); $cat++){
+        		$category  	= array_rand($categories);
+    			$article->addCategory($categories[$category]);
+        	}
+        	$article->setTitle($faker->name);
+        	$article->setContent($faker->realText(400));
+        	$article->setPublicationDate($faker->dateTime);
+        	$article->setLastUpdateDate($faker->dateTime);
+        	$article->setIsPublised(rand(0,1));      	
+        	$manager->persist($article);        	
+        }
+
+
+        $comment 	= new Comment(); 
+
+    	$manager->flush();
     }
 }
